20a8c4db1ffbba521ec536e2d4b02c7511ee6c79
ilmannafian04 <milmannafian04@gmail.com>
2022-01-04 10:27:37 +0700

    rip bozo

--- CodingCompanion/CodingCompanion/src/main.c
+++ CodingCompanion/CodingCompanion/src/main.c
@@ -36,11 +36,14 @@
 static char strbuf[128];
 
 #define LIGHT_Y 6 * 1
-#define LIGHT_THRESHOLD_MINOR 300
-#define LIGHT_THRESHOLD_MAJOR 100
-#define SIT_Y 6 * 10
+#define LIGHT_THRESHOLD_MINOR 100
+#define LIGHT_THRESHOLD_MAJOR 50
+#define SIT_Y 6 * 11
 #define SIT_THRESHOLD_MINOR 1
 #define SIT_THRESHOLD_MAJOR 2
+#define TEMP_Y 6 * 17
+#define TEMP_THRESHOLD_HOT 35
+#define TEMP_THRESHOLD_COLD 20
 
 // sensor results
 // light
@@ -58,21 +61,22 @@ enum severity
 
 enum severity light_severity = SEVERITY_OK;
 enum severity sit_severity = SEVERITY_OK;
+enum severity temp_severity = SEVERITY_OK;
 
 enum message_type
 {
 	MESSAGE_TYPE_NONE,
 	MESSAGE_TYPE_LIGHT,
-	MESSAGE_TYPE_SIT
+	MESSAGE_TYPE_SIT,
+	MESSAGE_TYPE_HOT,
+	MESSAGE_TYPE_COLD
 };
 
 enum message_type current_message = MESSAGE_TYPE_NONE;
 
-bool on = false;
-void update_actuator(void);
-void update_actuator()
+void update_sitting_duration(void);
+void update_sitting_duration()
 {
-	// sensor handling
 	// handle sitting duration
 	if (!ioport_get_pin_level(GPIO_PUSH_BUTTON_1))
 	{
@@ -82,144 +86,13 @@ void update_actuator()
 	{
 		button_pressed_duration = 0;
 	}
-
-	// display light intensity
-	uint32_t light_intensity = light_intensity_sum / light_intensity_count;
-	snprintf(strbuf, sizeof(strbuf), "%5lu", light_intensity);
-	gfx_mono_draw_string(strbuf, LIGHT_Y, 8, &sysfont);
-	// display sitting duration
-	// uint32_t sitting_duration = floor(button_pressed_duration / 3600);
-	uint32_t sitting_duration = button_pressed_duration;
-	snprintf(strbuf, sizeof(strbuf), "%2lu", sitting_duration);
-	gfx_mono_draw_string(strbuf, SIT_Y, 8, &sysfont);
-	// display face distance from monitor
-
-	// determine severity
-	// light severity
-	enum severity prev_light_severity = light_severity;
-	if (light_intensity < LIGHT_THRESHOLD_MINOR)
-	{
-		if (light_intensity > LIGHT_THRESHOLD_MAJOR)
-		{
-			light_severity = SEVERITY_MINOR;
-		}
-		else
-		{
-			light_severity = SEVERITY_MAJOR;
-		}
-	}
-	else
-	{
-		light_severity = SEVERITY_OK;
-	}
-	// sitting duration
-	enum severity prev_sit_severity = sit_severity;
-	if (sitting_duration >= SIT_THRESHOLD_MINOR)
-	{
-		if (sitting_duration < SIT_THRESHOLD_MAJOR)
-		{
-			sit_severity = SEVERITY_MINOR;
-		}
-		else
-		{
-			sit_severity = SEVERITY_MAJOR;
-		}
-	}
-	else
-	{
-		sit_severity = SEVERITY_OK;
-	}
-
-	// turn on led on warning
-	// light
-	if (light_severity != prev_light_severity)
-	{
-		if (light_severity > SEVERITY_OK)
-		{
-			ioport_set_pin_level(LED0_GPIO, IOPORT_PIN_LEVEL_LOW);
-		}
-		else
-		{
-			ioport_set_pin_level(LED0_GPIO, IOPORT_PIN_LEVEL_HIGH);
-		}
-	}
-	// sitting
-	if (sit_severity != prev_sit_severity)
-	{
-		if (sit_severity > SEVERITY_OK)
-		{
-			ioport_set_pin_level(LED1_GPIO, IOPORT_PIN_LEVEL_LOW);
-		}
-		else
-		{
-			ioport_set_pin_level(LED1_GPIO, IOPORT_PIN_LEVEL_HIGH);
-		}
-	}
-
-	// buzzer handling
-	if ((light_severity != prev_light_severity) || (sit_severity != prev_sit_severity))
-	{
-		if ((light_severity == SEVERITY_MAJOR) || (sit_severity == SEVERITY_MAJOR))
-		{
-			ioport_set_pin_level(J1_PIN0, IOPORT_PIN_LEVEL_HIGH);
-		}
-		else
-		{
-			ioport_set_pin_level(J1_PIN0, IOPORT_PIN_LEVEL_LOW);
-		}
-	}
-
-	// message handling
-	// decide what to display
-	enum message_type prev_message = current_message;
-	if (light_severity > SEVERITY_OK)
-	{
-		current_message = MESSAGE_TYPE_LIGHT;
-	}
-	else if (sit_severity > SEVERITY_OK)
-	{
-		current_message = MESSAGE_TYPE_SIT;
-	}
-	else
-	{
-		current_message = MESSAGE_TYPE_NONE;
-	}
-	// check whether to rewrite
-	if (current_message != prev_message)
-	{
-		if (current_message == MESSAGE_TYPE_LIGHT)
-		{
-			snprintf(strbuf, sizeof(strbuf), "%-21s", "Ambient is too dark");
-			gfx_mono_draw_string(strbuf, 0, 16, &sysfont);
-			snprintf(strbuf, sizeof(strbuf), "%-21s", "Turn on some light");
-			gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
-		}
-		else if (current_message == MESSAGE_TYPE_SIT)
-		{
-			snprintf(strbuf, sizeof(strbuf), "%-21s", "Sat for too long");
-			gfx_mono_draw_string(strbuf, 0, 16, &sysfont);
-			snprintf(strbuf, sizeof(strbuf), "%-21s", "Please stand up");
-			gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
-		}
-		else
-		{
-			snprintf(strbuf, sizeof(strbuf), "%-21s", "");
-			gfx_mono_draw_string(strbuf, 0, 16, &sysfont);
-			snprintf(strbuf, sizeof(strbuf), "%-21s", "");
-			gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
-		}
-	}
-
-	// reset light intensity
-	light_intensity_sum = 0;
-	light_intensity_count = 0;
 }
 
-void setup_timer(void);
-void setup_timer()
+void setup_sitting_timer(void);
+void setup_sitting_timer()
 {
 	tc_enable(&TCC0);
-	tc_set_overflow_interrupt_callback(&TCC0, update_actuator);
+	tc_set_overflow_interrupt_callback(&TCC0, update_sitting_duration);
 	tc_set_wgm(&TCC0, TC_WG_NORMAL);
 	tc_write_period(&TCC0, 2000);
 	tc_set_overflow_interrupt_level(&TCC0, TC_INT_LVL_LO);
@@ -244,7 +117,7 @@ int main(void)
 	delay_ms(1000);
 
 	// setup timer
-	setup_timer();
+	setup_sitting_timer();
 	cpu_irq_enable();
 
 	// setup adc
@@ -258,18 +131,201 @@ int main(void)
 
 	// print name and skeleton
 	gfx_mono_draw_string("Coding Companion", 0, 0, &sysfont);
-	gfx_mono_draw_string("L    0lx S 0h D   0cm", 0, 8, &sysfont);
+	gfx_mono_draw_string("L    0lx  S 0h  T  0c", 0, 8, &sysfont);
 
 	//forever loop
 	while (1)
 	{
-		// ROOM LIGHTNING TOO DIM
+		// sensor readings
+		// LIGHT
 		// get light intensity and wait until it is ready
 		lightsensor_measure();
 		while (!lightsensor_data_is_ready())
 		{
 		}
-		light_intensity_sum += lightsensor_get_raw_value();
-		light_intensity_count++;
+		// TEMP
+		// measure temp and wait until it is ready
+		ntc_measure();
+		while (!ntc_data_is_ready())
+		{
+		}
+
+		// display light intensity
+		uint32_t light_intensity = lightsensor_get_raw_value();
+		snprintf(strbuf, sizeof(strbuf), "%5lu", light_intensity);
+		gfx_mono_draw_string(strbuf, LIGHT_Y, 8, &sysfont);
+		// display sitting duration
+		// uint32_t sitting_duration = floor(button_pressed_duration / 3600);
+		uint32_t sitting_duration = button_pressed_duration;
+		snprintf(strbuf, sizeof(strbuf), "%2lu", sitting_duration);
+		gfx_mono_draw_string(strbuf, SIT_Y, 8, &sysfont);
+		// display room temperature
+		int8_t room_temperature = ntc_get_temperature();
+		snprintf(strbuf, sizeof(strbuf), "%3d", room_temperature);
+		gfx_mono_draw_string(strbuf, TEMP_Y, 8, &sysfont);
+
+		// determine severity
+		// light severity
+		enum severity prev_light_severity = light_severity;
+		if (light_intensity < LIGHT_THRESHOLD_MINOR)
+		{
+			if (light_intensity > LIGHT_THRESHOLD_MAJOR)
+			{
+				light_severity = SEVERITY_MINOR;
+			}
+			else
+			{
+				light_severity = SEVERITY_MAJOR;
+			}
+		}
+		else
+		{
+			light_severity = SEVERITY_OK;
+		}
+		// sitting duration
+		enum severity prev_sit_severity = sit_severity;
+		if (sitting_duration >= SIT_THRESHOLD_MINOR)
+		{
+			if (sitting_duration < SIT_THRESHOLD_MAJOR)
+			{
+				sit_severity = SEVERITY_MINOR;
+			}
+			else
+			{
+				sit_severity = SEVERITY_MAJOR;
+			}
+		}
+		else
+		{
+			sit_severity = SEVERITY_OK;
+		}
+		// room temperature
+		enum severity prev_temp_severity = temp_severity;
+		if (room_temperature > TEMP_THRESHOLD_HOT)
+		{
+			temp_severity = SEVERITY_MINOR;
+		}
+		else if (room_temperature < TEMP_THRESHOLD_COLD)
+		{
+			temp_severity = SEVERITY_MAJOR;
+		}
+		else
+		{
+			temp_severity = SEVERITY_OK;
+		}
+
+		// turn on led on warning
+		// light
+		if (light_severity != prev_light_severity)
+		{
+			if (light_severity > SEVERITY_OK)
+			{
+				ioport_set_pin_level(LED0_GPIO, IOPORT_PIN_LEVEL_LOW);
+			}
+			else
+			{
+				ioport_set_pin_level(LED0_GPIO, IOPORT_PIN_LEVEL_HIGH);
+			}
+		}
+		// sitting
+		if (sit_severity != prev_sit_severity)
+		{
+			if (sit_severity > SEVERITY_OK)
+			{
+				ioport_set_pin_level(LED1_GPIO, IOPORT_PIN_LEVEL_LOW);
+			}
+			else
+			{
+				ioport_set_pin_level(LED1_GPIO, IOPORT_PIN_LEVEL_HIGH);
+			}
+		}
+		// temperature
+		if (temp_severity != prev_temp_severity)
+		{
+			if (temp_severity > SEVERITY_OK)
+			{
+				ioport_set_pin_level(LED2_GPIO, IOPORT_PIN_LEVEL_LOW);
+			}
+			else
+			{
+				ioport_set_pin_level(LED2_GPIO, IOPORT_PIN_LEVEL_HIGH);
+			}
+		}
+
+		// buzzer handling
+		if ((light_severity != prev_light_severity) || (sit_severity != prev_sit_severity))
+		{
+			if ((light_severity == SEVERITY_MAJOR) || (sit_severity == SEVERITY_MAJOR))
+			{
+				ioport_set_pin_level(J1_PIN0, IOPORT_PIN_LEVEL_HIGH);
+			}
+			else
+			{
+				ioport_set_pin_level(J1_PIN0, IOPORT_PIN_LEVEL_LOW);
+			}
+		}
+
+		// message handling
+		// decide what to display
+		enum message_type prev_message = current_message;
+		if (light_severity > SEVERITY_OK)
+		{
+			current_message = MESSAGE_TYPE_LIGHT;
+		}
+		else if (sit_severity > SEVERITY_OK)
+		{
+			current_message = MESSAGE_TYPE_SIT;
+		}
+		else if (temp_severity == SEVERITY_MINOR)
+		{
+			current_message = MESSAGE_TYPE_HOT;
+		}
+		else if (temp_severity == SEVERITY_MAJOR)
+		{
+			current_message = MESSAGE_TYPE_COLD;
+		}
+		else
+		{
+			current_message = MESSAGE_TYPE_NONE;
+		}
+		// check whether to rewrite
+		if (current_message != prev_message)
+		{
+			if (current_message == MESSAGE_TYPE_LIGHT)
+			{
+				snprintf(strbuf, sizeof(strbuf), "%-21s", "Ambient is too dark");
+				gfx_mono_draw_string(strbuf, 0, 16, &sysfont);
+				snprintf(strbuf, sizeof(strbuf), "%-21s", "Turn on some light");
+				gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
+			}
+			else if (current_message == MESSAGE_TYPE_SIT)
+			{
+				snprintf(strbuf, sizeof(strbuf), "%-21s", "Sat for too long");
+				gfx_mono_draw_string(strbuf, 0, 16, &sysfont);
+				snprintf(strbuf, sizeof(strbuf), "%-21s", "Please stand up");
+				gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
+			}
+			else if (current_message == MESSAGE_TYPE_HOT)
+			{
+				snprintf(strbuf, sizeof(strbuf), "%-21s", "Room is too hot");
+				gfx_mono_draw_string(strbuf, 0, 16, &sysfont);
+				snprintf(strbuf, sizeof(strbuf), "%-21s", "Please turn on AC");
+				gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
+			}
+			else if (current_message == MESSAGE_TYPE_COLD)
+			{
+				snprintf(strbuf, sizeof(strbuf), "%-21s", "Room is too cold");
+				gfx_mono_draw_string(strbuf, 0, 16, &sysfont);
+				snprintf(strbuf, sizeof(strbuf), "%-21s", "Please turn off AC");
+				gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
+			}
+			else
+			{
+				snprintf(strbuf, sizeof(strbuf), "%-21s", "");
+				gfx_mono_draw_string(strbuf, 0, 16, &sysfont);
+				snprintf(strbuf, sizeof(strbuf), "%-21s", "");
+				gfx_mono_draw_string(strbuf, 0, 24, &sysfont);
+			}
+		}
 	}
 }
